02a87a74d7eea1dd05d1376b99cc4dcc2bd28021
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2016-02-10 21:50:26 -0700

    Refactor everything

--- mytransit/app.py
+++ mytransit/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
 
@@ -9,51 +9,72 @@ from transit import get_gtfs_data
 from transit import get_markers_for_list_entities
 from transit import get_real_time_data_request_response
 
+import json
+from collections import namedtuple
 import datetime
+import os
 
 from helper import merge_two_dicts
 
 app = Flask(__name__, template_folder="./templates")
 GoogleMaps(app)
 
-@app.route("/")
-def mapview():
-    # creating a map in the view
-    
+@app.route("/data")
+def data():
+
+    print("before gtfs - {}".format(datetime.datetime.now()))
     get_gtfs_data()
-# 
+    print("after gtfs - {}".format(datetime.datetime.now()))
+
+    print("before csv - {}".format(datetime.datetime.now()))
     bus20_east_df, bus20_west_df, stops_df = get_bus_data_from_csv()
-# 
+    print("after csv - {}".format(datetime.datetime.now()))
+
+    print("before list - {}".format(datetime.datetime.now()))
     bus20_east_list = convert_df_to_list(bus20_east_df)
     bus20_west_list = convert_df_to_list(bus20_west_df)
-# 
-    _, headers = get_real_time_data_request_response()
-    last_modified = headers['Last-Modified']
+    print("after list - {}".format(datetime.datetime.now()))
 
+    print("before header - {}".format(datetime.datetime.now()))
+    headers = get_real_time_data_request_response(header=True)
+    last_modified = headers['Last-Modified']
+    print("after header - {}".format(datetime.datetime.now()))
+
+    print("before entities - {}".format(datetime.datetime.now()))
     l1 = get_entities(bus20_east_list)
     l2 = get_entities(bus20_west_list)
+    print("after entities - {}".format(datetime.datetime.now()))
 
-    bus_20_east_dict = {'http://maps.google.com/mapfiles/ms/icons/green-dot.png': get_markers_for_list_entities(l1, stops_df),
+    bus_20_east_dict = {'/static/transit-east.png': get_markers_for_list_entities(l1, stops_df),
                }
-    bus_20_west_dict = {'http://maps.google.com/mapfiles/ms/icons/blue-dot.png': get_markers_for_list_entities(l2, stops_df),
+    bus_20_west_dict = {'/static/transit-west.png': get_markers_for_list_entities(l2, stops_df),
                  }
 
     markers = merge_two_dicts(bus_20_east_dict, bus_20_west_dict)
 
-    mymap = Map(
-        identifier="view-side",
-        lat=39.7392,
-        lng=-104.9903,
-        markers=markers
-    )
+    lat_lng = {'lat': 39.7392, 'lng': -104.9903} # Denver downtown
 
     UTC_OFFSET = 7
+
     dt = datetime.datetime.strptime(last_modified, '%a, %d %b %Y %H:%M:%S GMT')
     dt = dt - datetime.timedelta(hours=UTC_OFFSET)
     last_modified = dt.strftime('%a, %d %b %Y %H:%M:%S MST')
 
-    return render_template('map.html', mymap=mymap, last_modified=last_modified)
+    data = {'last_modified': last_modified,
+            'location': lat_lng,
+            'markers': markers}
+
+    # data = {"last_modified": "Wed, 10 Feb 2016 20:13:39 MST", "location": {"lat": 39.7434915, "lng": -104.9890398}, "markers": {"/static/transit-west.png": [], "/static/transit-east.png": [[39.7434915, -104.9890398]]}}
+
+    return json.dumps(data)
+
+@app.route("/")
+def mapview():
+    # creating a map in the view
+
+    return render_template('map.html', json_api_key=os.getenv('JSON_API'))
 
 if __name__ == "__main__":
-    app.debug = False
+    app.debug = os.getenv('DEBUG', False)
+    app.threaded = os.getenv('THREADED', False)
     app.run()
--- mytransit/transit.py
+++ mytransit/transit.py
@@ -1,17 +1,43 @@
+from __future__ import print_function
 from google.transit import gtfs_realtime_pb2
-
 import requests
 import pandas as pd
 from io import BytesIO
 import zipfile
 import os
+import os.path
+import datetime
 
 def get_gtfs_data():
     url = 'http://www.rtd-denver.com/GoogleFeeder/google_transit_Jan16_Runboard.zip'
-    request = requests.get(url)
-    z = zipfile.ZipFile(BytesIO(request.content))
-    z.extractall()
-    return z
+    headers_file = 'google_feeder_headers.txt'
+
+    last_modified = requests.head(url).headers['Last-Modified']
+    rerequest = False
+
+    if not os.path.isfile(headers_file):
+        rerequest = True
+    else:
+        with open(headers_file) as f:
+            f_line = f.read()
+            if last_modified not in f_line:
+                print("File are not the same")
+                rerequest = True
+            else:
+                print("File unchanged!")
+                if not os.path.isfile('stops.txt'):
+                    rerequest = True
+                    print("Files missing")
+
+    if rerequest:
+        print("Re-requesting data")
+        request = requests.get(url)
+        z = zipfile.ZipFile(BytesIO(request.content))
+        z.extractall()
+        with open(headers_file, 'w') as f:
+            print(last_modified, file=f)
+        return z
+                
 
 def get_bus_data_from_csv():
 
@@ -32,16 +58,22 @@ def convert_df_to_list(df):
     # bus20_west_list = [str(i) for i in bus20_west_df['trip_id'].tolist()]
     return([str(i) for i in df['trip_id'].tolist()])
 
-def get_real_time_data_request_response():
-    r = requests.get('http://www.rtd-denver.com/google_sync/TripUpdate.pb', auth=(os.getenv('RTD_USERNAME'), os.getenv('RTD_PASSWORD')))
-    if r.ok:
-        return(r.content, r.headers)
+def get_real_time_data_request_response(header=False):
+    if header:
+        r = requests.head('http://www.rtd-denver.com/google_sync/TripUpdate.pb', auth=(os.getenv('RTD_USERNAME'), os.getenv('RTD_PASSWORD')))
+        return(r.headers)
+    else:
+        r = requests.get('http://www.rtd-denver.com/google_sync/TripUpdate.pb', auth=(os.getenv('RTD_USERNAME'), os.getenv('RTD_PASSWORD')))
+        if r.ok:
+            return(r.content)
+        else:
+            return None
 
 def get_entities(bus_list):
 
     feed = gtfs_realtime_pb2.FeedMessage()
 
-    content, _ = get_real_time_data_request_response()
+    content = get_real_time_data_request_response()
 
     feed.ParseFromString(content)
 
@@ -57,9 +89,17 @@ def get_markers_for_list_entities(list_entities, stops_df):
     marker = []
     for entity in list_entities:
         stop_time_update = entity.trip_update.stop_time_update[0]
+        delay = stop_time_update.departure.delay
+        uncertainty = stop_time_update.departure.uncertainty
+        UTC_OFFSET = 7
+        dt = datetime.datetime.fromtimestamp(stop_time_update.departure.time)
+        dt = dt - datetime.timedelta(hours=UTC_OFFSET)
+        departure_time = dt.strftime('%H:%M')
+
         lat = stops_df[stops_df['stop_id']==int(stop_time_update.stop_id)]['stop_lat'].iloc[0]
         lon = stops_df[stops_df['stop_id']==int(stop_time_update.stop_id)]['stop_lon'].iloc[0]
-        marker.append((lat, lon))
+        stop_name = stops_df[stops_df['stop_id']==int(stop_time_update.stop_id)]['stop_name'].iloc[0].replace('[ X Stop ]', '')
+        marker.append((lat, lon, stop_name, departure_time, delay, uncertainty))
 
     return marker
 
